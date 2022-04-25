72351f1655e08c8579c0d4edb26484e4ba65e2a4
mclds <mclds@protonmail.com>
2022-04-25 07:05:59 +0100

    25-04-2022 commit

--- pwd_python/int_search.py
+++ pwd_python/int_search.py
@@ -1,5 +1,3 @@
-import sys
-import traceback
 from mysql.connector import connect, Error
 from colr import color
 import fire
@@ -7,34 +5,27 @@ import fire
 
 def search_int():
     try:
-        busca = input(color(' What is the id? ', fore='#fe7243'))
-        conn = connect(
-                host="localhost",
-                user="mic",
-                password="xxxx",
-                database="pwd")
+        busca = input(color(" What is the id? ", fore="#fe7243"))
+        conn = connect(host="localhost", user="mic", password="xxxx", database="pwd")
         cur = conn.cursor()
         query = " SELECT pwdid, site, username, passwd, comment, time FROM pwd WHERE pwdid = " + busca
         cur.execute(query)
         records = cur.fetchall()
         for row in records:
-            print(color(' [0] ID » ', fore='#3c828e'), color(str(row[0]), fore='#efb666'))
-            print(color(' [1] SITE » ', fore='#3c828e'), color(str(row[1]), fore='#efb666'))
-            print(color(' [2] USERNAME » ', fore='#3c828e'), color(str(row[2]), fore='#efb666'))
-            print(color(' [3] PASSWORD » ', fore='#3c828e'), color(str(row[3]), fore='#efb666'))
-            print(color(' [4] COMMENT » ', fore='#3c828e'), color(str(row[4]), fore='#efb666'))
-            print(color(' [5] TIME : ', fore='#3c828e'), color(str(row[5]), fore='#efb666'))
-            print('\n')
+            print(color(" [0] ID » ", fore="#3c828e"), color(str(row[0]), fore="#efb666"))
+            print(color(" [1] SITE » ", fore="#3c828e"), color(str(row[1]), fore="#efb666"))
+            print(color(" [2] USERNAME » ", fore="#3c828e"), color(str(row[2]), fore="#efb666"))
+            print(color(" [3] PASSWORD » ", fore="#3c828e"), color(str(row[3]), fore="#efb666"))
+            print(color(" [4] COMMENT » ", fore="#3c828e"), color(str(row[4]), fore="#efb666"))
+            print(color(" [5] TIME : ", fore="#3c828e"), color(str(row[5]), fore="#efb666"))
+            print("\n")
     except Error as e:
         print("Error while connecting to db", e)
-        print("Exception class is: ", e.__class__)
-        print('SQLite traceback: ')
-        exc_type, exc_value, exc_tb = sys.exc_info()
-        print(traceback.format_exception(exc_type, exc_value, exc_tb))
+
     finally:
-        if(conn):
+        if conn:
             conn.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     fire.Fire(search_int)
--- pwd_python/search.py
+++ pwd_python/search.py
@@ -8,29 +8,24 @@ import fire
 
 def search():
     try:
-        busca = input(color(' What are you searching for? ', fore='#fe7243'))
-        conn = connect(
-                host="localhost",
-                user="mic",
-                password="xxxx",
-                database="pwd")
+        busca = input(color(" What are you searching for? ", fore="#fe7243"))
+        conn = connect(host="localhost", user="mic", password="xxxx", database="pwd")
         cur = conn.cursor()
-        query = " SELECT pwdid, site, username, passwd, comment, time FROM pwd WHERE MATCH(site, username, comment) AGAINST ('" \
-                + busca + "' IN NATURAL LANGUAGE MODE)"
+        query = " SELECT pwdid, site, username, passwd, comment, time FROM pwd WHERE MATCH(site, username, comment) AGAINST ('" + busca + "' IN NATURAL LANGUAGE MODE)"
         cur.execute(query)
         records = cur.fetchall()
-        headers = ['id', 'site', 'user', 'pwd', 'note', 'timestamp']
-        print(tabulate(records, headers, tablefmt='fancy_grid'))
+        headers = ["id", "site", "user", "pwd", "note", "timestamp"]
+        print(tabulate(records, headers, tablefmt="fancy_grid"))
     except Error as e:
         print("Error while connecting to db", e)
         print("Exception class is: ", e.__class__)
-        print('MySQL traceback: ')
+        print("MySQL traceback: ")
         exc_type, exc_value, exc_tb = sys.exc_info()
         print(traceback.format_exception(exc_type, exc_value, exc_tb))
     finally:
-        if(conn):
+        if conn:
             conn.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     fire.Fire(search)
